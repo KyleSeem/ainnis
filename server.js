b8244ed7412896fedb6bb88c0b0a1d9bb7a864da
Kyle Seem <kyle.seem@gmail.com>
2017-12-17 10:08:25 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static(path.resolve('public')));
